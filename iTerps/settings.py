0d3dd6b0ee6b68edceafe087544dc982c038c1c3
Bryan <33406715+BDubon@users.noreply.github.com>
2019-11-23 03:30:56 -0500

    Added django-heroku

--- iTerps/settings.py
+++ iTerps/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -167,3 +168,6 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     "django.core.context_processors.media",
     "django.core.context_processors.request",
 )
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
